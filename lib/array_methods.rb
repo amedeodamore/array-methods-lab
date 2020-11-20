0032a9e38aeb9c27d0613288ba58d25c072a170f
Amedeo d'Amore <amedeodamore@gmail.com>
2020-11-20 16:59:33 +0000

    commit tree 61fea4cffff13d82fd94759ccbdcfcd091c5272e

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,28 @@
 def using_include(array, element)
-
+  array.include?(element)
 end
 
 def using_sort(array)
-
+    famous_cats = ["lil' bub", "I", "wow", "maru"]
+    famous_cats.sort
 end
 
 def using_reverse(array)
-
+    famous_cats = ["wow", "I", "love", "arrays!"]
+    famous_cats.reverse
 end
 
 def using_first(array)
-
+    famous_cats = ["wow", "I", "love", "arrays!"]
+    famous_cats.first
 end
 
 def using_last(array)
-
+    famous_cats = ["wow", "I", "love", "arrays!"]
+    famous_cats.last
 end
 
 def using_size(array)
-
+    famous_cats = ["wow", "I", "love", "apple", "oreo", "arrays!"]
+    famous_cats.size
 end
